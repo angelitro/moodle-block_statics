637624d59d59c8df0147747e46575e5e265f2f94
angelitro <angelluisfraile@gmail.com>
2018-10-25 16:27:13 +0200

    5

--- db/events.php
+++ db/events.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
  
 /**
- * @package   block_mimodu
+ * @package   block_statics
  * @copyright 2018, angelitr0 <angel@angelitro.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
